b36b116194eb87a2f15964eb09b782cc0a145782
Adeilson Leal <adeilsonleal.al@gmail.com>
2025-05-23 13:05:51 -0300

    update

--- rotas/pratos.js
+++ rotas/pratos.js
@@ -5,16 +5,20 @@ const pratosRouters = express.Router(); // Cria uma instância da aplicação Ex
 
 // Rota para listar pratos
 pratosRouters.get('/', async (req, res) => {
-    const categoria = req.query.categoria;
+    const categoria = req.query.categoria; // Recebe valor a ser requisitado no banco de dados.
 
-    if(categoria === undefined){
+    if(categoria === undefined){ // Valida se a chave foi recebida corretamente.
         res.status(400).json({ Erro: 'Nenhum parametro recebido' }); // 
     }else {
         const listaPratos =  await retornaCategoriaPratos(categoria); 
-         res.status(200).json({ Pratos: listaPratos }); // 
+
+        if(listaPratos.status){
+            res.status(200).json(listaPratos.pratos);
+        }else{
+            res.status(401).json(listaPratos.pratos);
+        }
     }
 
-    
 });
 
 export default pratosRouters;
--- servicos/manipulaPratos.js
+++ servicos/manipulaPratos.js
@@ -1,16 +1,24 @@
 import pool from "./conexao.js"; // Importa o pool de conexões do arquivo conexao.js.
 
-// Função retorna categoria de pratos
+// Função retorna categoria de pratos.
 export async function retornaCategoriaPratos(categoria) {
     let conexao;
+    let status;
 
     try {
         conexao = await pool.getConnection(); // Obtém uma conexão do pool.
 
         // Executa a query de seleção no banco de dados.
-        const [pratos] = await conexao.query('SELECT categoria, nome, preco FROM cardapio WHERE categoria ="'+categoria+'"');
+        let [pratos] = await conexao.query('SELECT categoria, nome, preco FROM cardapio WHERE categoria ="' + categoria + '"');
 
-        return pratos; // Retorna a lista de pratos.
+        if (pratos.length > 0) { // Verifica se a bsuca encontrou algun dado.
+            status=true; // Seta status para verdadeiro.
+            return {pratos, status} // Retorna dados encontrados e status.
+        }else{
+            status=false; // Seta status para false.
+            pratos = {erro: 'Nenhum registro encontrado'}; // Informa que nenhum dado foi encontrado.
+            return {pratos, status} // Retorna mensagem de alerta e status.
+        }
 
     } catch (erro) {
         console.error("Erro ao buscar pratos:", erro);
